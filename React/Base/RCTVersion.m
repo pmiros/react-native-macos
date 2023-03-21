8f0d7fca693fade989cf26fe27fff51bcda5f4be
Adam Gleitman <adam.gleitman@gmail.com>
2023-03-21 15:49:15 -0700

    Manual bump to 0.68.67 (#1763)
    * Manual bump to 0.68.67
    
    * Update Podfile.lock
    
    ---------
    
    Co-authored-by: Adam Gleitman <adgleitm@microsoft.com>

--- React/Base/RCTVersion.m
+++ React/Base/RCTVersion.m
@@ -27,7 +27,7 @@ NSDictionary* RCTGetReactNativeVersion(void)
     __rnVersion = @{
                   RCTVersionMajor: @(0),
                   RCTVersionMinor: @(68),
-                  RCTVersionPatch: @(66),
+                  RCTVersionPatch: @(67),
                   RCTVersionPrerelease: [NSNull null],
                   };
   });
