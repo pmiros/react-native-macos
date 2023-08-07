5f840bb4646a570c25565bdfab652df65972f57c
Adam Gleitman <adgleitm@microsoft.com>
2023-08-07 15:15:35 -0700

    Add some debugging information

--- scripts/cocoapods/codegen.rb
+++ scripts/cocoapods/codegen.rb
@@ -15,6 +15,13 @@ def build_codegen!(react_native_path, relative_installation_root, dir_manager: D
     codegen_npm_path = "#{relative_installation_root}/#{react_native_path}/../@react-native/codegen";
     codegen_cli_path = ""
 
+    # Temporary debugging information
+    puts caller
+    puts react_native_path
+    puts relative_installation_root
+    puts codegen_repo_path
+    puts codegen_npm_path
+
     if dir_manager.exist?(codegen_repo_path)
       codegen_cli_path = codegen_repo_path
     elsif dir_manager.exist?(codegen_npm_path)
